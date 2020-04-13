c9ef62918af9d9fdb27bacac6d6ddfed86e99c9f
iqmalakbarkurnia <iakbarkurnia@gmail.com>
2020-04-13 09:30:41 +0700

    Menambahkan Gallery

--- index.php
+++ index.php
@@ -1,159 +1,154 @@
-<?php
-    require "function/fungsi.php";
-
-    $image = query("SELECT * FROM img");
-    $berita = query("SELECT * FROM berita");
-?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- My CSS -->
-    <link rel="stylesheet" href="css/style.css">
+<html lang="en" id="top">
 
-    <title>Home</title>
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+
+<!-- My CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>Elite Archer</title>
 </head>
-<body id="top">
+
+<body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#top">
-                <img src="img/logo/target2.png" width="30" height="30" class="d-inline-block align-top"
-                    alt="logo">
-                Archery
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                            aria-expanded="false">Dropdown</a>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item pageScroll" href="#info">Info</a>
-                            <a class="dropdown-item pageScroll" href="#gallery">Gallery</a>
-                            <a class="dropdown-item pageScroll" href="#berita">Berita</a>
-                            <a class="dropdown-item pageScroll" href="#contact">Contact Us</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Daftar</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pendataan</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Laporan Data</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Jadwal</a>
-                    </li>
-                </ul>
+        <a class="navbar-brand pageScroll" href="#top"><img src="img/logo/target2.png" alt="logo" class="logo">Elite Archer</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+            <a class="nav-item nav-link" href="#">Pendaftaran</a>
+            <a class="nav-item nav-link" href="#">Jadwal Kegiatan</a>
+            <a class="nav-item nav-link" href="#" tabindex="-1" aria-disabled="true">Kepengurusan</a>
+            <a class="nav-item btn btn-primary tombol" href="#" tabindex="-1" aria-disabled="true">Join Us</a>
             </div>
+        </div>
         </div>
     </nav>
+    <!-- Akhir Navbar -->
 
     <!-- Jumbotron -->
-    <div class="jumbotron">
+    <div class="jumbotron jumbotron-fluid">
         <div class="container">
-            <h1 class="display-4 text-center judul">Olahraga Panahan</h1>
-            <p class="lead text-center">Lebih Berani, Lebih Percaya Diri</p>
-            <br>
-            <p>Tepat Sasaran itu Utama</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Daftar Sekarang <img src="img/logo/target1.png" width="30" height="30" class="d-inline-block align-top" alt="logo"></a>
+            <h1 class="display-4">Right on <span>Target</span><br> and <span>Confident</span> with us</h1>
+            <a href="" class="btn btn-primary tombol">Daftar Sekarang<img src="img/logo/target1.png" alt="logo" class="logo"></a>
         </div>
     </div>
-
-    <!-- Info -->
-    <section class="container" id="info">
-        <h1 class="text-center judul">Information</h1>
-        <hr class="garis">
-        <div class="row">
-            <div class="col-md-4 offset-md-2">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam autem, cupiditate, blanditiis quo minima molestiae aspernatur, modi reiciendis dolorum odio accusantium incidunt aut perspiciatis quibusdam quia laudantium laboriosam exercitationem distinctio?</p>
-            </div>
-            <div class="col-md-4">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim excepturi dolores corrupti quasi molestias sint iste similique quidem molestiae sed cum asperiores minus neque, ipsum hic pariatur. Aspernatur, facilis iusto!</p>
-            </div>
-        </div>
-    </section>
-
-    <!-- Gallery -->
-    <section class="container" id="gallery">
-        <div class="container">
+    <!-- Akhir Jumbotron -->
+    
+    <!-- Container -->
+    <div class="container">
+        <!-- Info Panel -->
+        <div class="row justify-content-center">
+        <div class="col-10 info-panel">
             <div class="row">
-                <div class="col-sm-12 text-center">
-                    <h1 class="text-center judul">Gallery</h1>
-                    <hr class="garis">
-                </div>
+            <div class="col colinfo">
+                <img src="img/logo/bow.png" alt="bow" class="float-left">
+                <h4>Perlombaan</h4>
+                <p>Kami selalu berpatisipasi dalam berbagai Perlombaan Archery</p>
             </div>
-            <div class="row text-center">
-                <?php foreach($image as $img): ?>
-                    <div class="col-sm-4">
-                        <a href="#" class="thumbnail"><img src="img/gallery/<?= $img['img'] ?>" alt="<?= $img['caption'] ?>" class="thumb">
-                        <br>
-                        <p><?= $img['caption']; ?></p></a>
-                    </div>
-                <?php endforeach ?>
+            <div class="col colinfo">
+                <img src="img/logo/pelatih.png" alt="pelatih" class="float-left">
+                <h4>Pelatih Profesional</h4>
+                <p>Pelatih memiliki pengalaman yang baik dalam bidang Archery</p>
+            </div>
+            <div class="col colinfo">
+                <img src="img/logo/fasilitas.png" alt="fasilitas" class="float-left">
+                <h4>Fasilitas</h4>
+                <p>Kami memiliki lapangan panah sendiri dan fasilitas lainnya</p>
+            </div>
             </div>
         </div>
-    </section>
+        </div>
+        <!-- Akhir Info Panel -->
 
-    <!-- Berita -->
-    <section class="container" id="berita">
-        <h1 class="text-center judul">Berita</h1>
-        <hr class="garis">
-        <?php foreach($berita as $b): ?>
-            <div class="row berita">
-                <div class="col-md-3 offset-1"><img src="img/berita/<?= $b['gambar']; ?>" alt="news"></div>
-                <div class="col-md-7"><?= $b['isi']; ?></div>
+        <!-- Informasi -->
+        <section class="container info">
+            <h1 class="judul text-center mb-5">Apa itu Archery?</h1>
+            <div class="row content">
+                <div class="col-sm-6 pkiri">Menurut <a href="https://id.wikipedia.org/wiki/Panahan">Wikipedia</a> Panahan (Inggris:Archery) adalah suatu kegiatan menggunakan busur panah untuk menembakkan anak panah. Bukti-bukti menunjukkan bahwa sejarah panahan telah dimulai sejak 5.000 tahun yang lalu yang awalnya digunakan untuk berburu dan kemudian berkembang sebagai senjata dalam pertempuran dan kemudian sebagai olahraga ketepatan. Seseorang yang gemar atau merupakan ahli dalam memanah disebut juga sebagai Pemanah atau dalam bahasa Inggris disebut Archer.</div>
+                <div class="col-sm-6 pkanan">Selain itu, Sebagai salah satu cabang olahraga modern,  bagi umat Islam, panahan menempati salah satu posisi olahraga yang istimewa. Seperti yang dikutip dari <a href="https://republika.co.id/berita/nwgu9i313/alasan-panahan-tempati-posisi-istimewa-dalam-islam">republika.co.id</a>, Panahan merupakan salah satu olahraga yang dianjurkan oleh Rasulullah SAW.<br>Rasulullah bersabda, "Ajarilah anak-anak kalian berkuda, berenang, dan memanah," (HR Bukhari/Muslim).<br>Melihat hadis tersebut, jelas sekali bahwa olahraga memanah memiliki kaitan erat dengan peradaban Islam.</div>
             </div>
-        <?php endforeach ?>
-    </section>
+        </section>
+        <!-- Akhir Informasi -->
 
-    <!-- Contact Us -->
-    <section class="container" id="contact">
-        <h1 class="text-center judul">Contact Us</h1>
-        <hr class="garis">
-        <form action="" method="post">
-            <div class="form-group row">
-                <label for="nama" class="col-sm-2 col-form-label col-form-label-lg">Nama</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control form form-control-lg" id="nama" name="nama" required="true">
-                </div>
+        <!-- Gallery -->
+        <section class="container gallery">
+            <h1 class="judul text-center mb-5">Gallery Kegiatan</h1>
+            <div class="row">
+                <figure class="col-md-3 figure">
+                    <img src="img/gallery/gallery1.jpg" class="figure-img img-fluid rounded" alt="gallery">
+                    <figcaption class="figure-caption">Latihan Bersama</figcaption>
+                </figure>
+                <figure class="col-md-3 figure">
+                    <img src="img/gallery/gallery2.jpg" class="figure-img img-fluid rounded" alt="gallery">
+                    <figcaption class="figure-caption">Grup Pemanah Wanita</figcaption>
+                </figure>
+                <figure class="col-md-3 figure">
+                    <img src="img/gallery/gallery3.jpg" class="figure-img img-fluid rounded" alt="gallery">
+                    <figcaption class="figure-caption">Diawasi Pelatih Profesional</figcaption>
+                </figure>
+                <figure class="col-md-3 figure">
+                    <img src="img/gallery/gallery4.png" class="figure-img img-fluid rounded" alt="gallery">
+                    <figcaption class="figure-caption">Belajar Cara Memanah</figcaption>
+                </figure>
             </div>
-            <div class="form-group row">
-                <label for="email" class="col-sm-2 col-form-label col-form-label-lg">Email</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control form form-control-lg" id="email" name="email" required="true">
-                </div>
+            <div class="row">
+                <figure class="col-md-3 figure">
+                    <img src="img/gallery/gallery5.jpg" class="figure-img img-fluid rounded" alt="gallery">
+                    <figcaption class="figure-caption">Pemanah Senior Pria</figcaption>
+                </figure>
+                <figure class="col-md-3 figure">
+                    <img src="img/gallery/gallery6.jpg" class="figure-img img-fluid rounded" alt="gallery">
+                    <figcaption class="figure-caption">Pemanah Senior Wanita</figcaption>
+                </figure>
+                <figure class="col-md-3 figure">
+                    <img src="img/gallery/gallery7.jpeg" class="figure-img img-fluid rounded" alt="gallery">
+                    <figcaption class="figure-caption">Latihan Bersama Teman Pemanah</figcaption>
+                </figure>
+                <figure class="col-md-3 figure">
+                    <img src="img/gallery/gallery8.jpg" class="figure-img img-fluid rounded" alt="gallery">
+                    <figcaption class="figure-caption">Pemanah Anak-Anak</figcaption>
+                </figure>
             </div>
-            <div class="form-group row">
-                <div class="col-sm-12">
-                    <textarea name="pesan" class="form-control form form-control-lg"
-                        placeholder="Masukkan Pesan..." rows="5" required="true"></textarea>
-                </div>
-            </div>
-            <button type="submit" name="komentar" class="btn btn-success mybtn">Kirim Komentar</button>
-        </form>
-    </section>
+            <div class="galleryfocus"><img src="" alt="gallery"></div>
+        </section>
+        <!-- Akhir Gallery -->
 
+        <!-- Berita -->
+        <section class="berita">
+            <h1 class="judul text-center mb-5">Berita</h1>
+            
+        </section>
+        <!-- Akhir Berita -->
+    </div>
+    <!-- Akhir Container -->
+    
     <!-- Footer -->
-    <footer class="text-center bg-dark text-light">
-        &#169; Copyright 2020 | Perpustakaankuuu
+    <footer class="copyright bg-dark text-center">
+        <span>&#169; Copyright Elite Archer 2020 | Iqmal Akbar Kurnia</span>
     </footer>
+    <!-- Akhir Footer -->
 
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery-3.4.1.slim.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- My JavaScript -->
-    <script src="js/script.js"></script>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
 </body>
+
 </html>
--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 // Parallax
-$(window).on('load', function () {
-    $('.pKiri').addClass('pMuncul')
-    $('.pKanan').addClass('pMuncul')
+$(window).on('load', function() {
+    let wScroll = $(this).scrollTop()
+    
+    if (wScroll > 0) {
+        $('.colinfo').each(function (i) {
+            setTimeout(function () {
+                $('.colinfo').eq(i).addClass('muncul')
+            }, 300 * i);
+        })
+    }
+
+    if (wScroll > $('section.info').offset().top - 300) {
+        $('nav.navbar').addClass('bg-dark')
+        $('section.info .judul').addClass('muncul')
+        $('section.info .pkiri').addClass('muncul')
+        $('section.info .pkanan').addClass('muncul')
+    }
+
+    if (wScroll > $('section.gallery').offset().top - 300) {
+        $('section.gallery .judul').addClass('muncul')
+        $('.figure').each(function (i) {
+            setTimeout(function () {
+                $('.figure').eq(i).addClass('muncul')
+            }, 300 * i);
+        })
+    }
 })
 
 $(window).scroll(function () {
@@ -10,23 +33,85 @@ $(window).scroll(function () {
 
     // Jumbotron
     $('.jumbotron h1').css({
-        'transform': 'translate(0,' + (wScroll / 0.8) + '%)'
-    })
-
-    $('.jumbotron p').css({
-        'transform': 'translate(0,' + (wScroll / 0.4) + '%)'
+        'transform': 'translate(0,' + (wScroll / 5) + '%)'
     })
 
     $('.jumbotron a').css({
-        'transform': 'translate(0,' + (wScroll / 1) + '%)'
+        'transform': 'translate(0,' + (wScroll / 5.2) + '%)'
     })
 
-    // Gallery
-    if (wScroll > ($('.gallery').offset().top - 250)) {
-        $('.gallery .thumbnail').each(function (i) {
+    if(wScroll > 0){
+        $('.colinfo').each(function (i) {
             setTimeout(function () {
-                $('.gallery .thumbnail').eq(i).addClass('muncul')
+                $('.colinfo').eq(i).addClass('muncul')
+            }, 300 * i);
+        })
+    } else if(wScroll <= 0){
+        $('.colinfo').each(function (i) {
+            setTimeout(function () {
+                $('.colinfo').eq(i).removeClass('muncul')
             }, 300 * i);
         })
     }
+
+    if (wScroll > $('section.info').offset().top - 300){
+        $('nav.navbar').addClass('bg-dark')
+        $('section.info .judul').addClass('muncul')
+        $('section.info .pkiri').addClass('muncul')
+        $('section.info .pkanan').addClass('muncul')
+    } else if (wScroll <= $('section.info').offset().top - 300) {
+        $('nav.navbar').removeClass('bg-dark')
+        $('section.info .judul').removeClass('muncul')
+        $('section.info .pkiri').removeClass('muncul')
+        $('section.info .pkanan').removeClass('muncul')
+    }
+
+    if (wScroll > $('section.gallery').offset().top - 300) {
+        $('section.gallery .judul').addClass('muncul')
+        $('.figure').each(function (i) {
+            setTimeout(function () {
+                $('.figure').eq(i).addClass('muncul')
+            }, 300 * i);
+        })
+    } else if (wScroll <= $('section.gallery').offset().top - 500) {
+        $('section.gallery .judul').removeClass('muncul')
+        $('.figure').removeClass('muncul')
+    }
+
+    if (wScroll > $('section.berita').offset().top - 300) {
+        $('section.berita .judul').addClass('muncul')
+    } else if (wScroll <= $('section.berita').offset().top - 500) {
+        $('section.berita .judul').removeClass('muncul')
+    }
+})
+
+// Page Scroll
+$('.pageScroll').on('click', function (e) {
+    const tujuan = $(this).attr('href')
+    const elementTujuan = $(tujuan)
+
+    $('html').animate({
+        scrollTop: elementTujuan.offset().top - 50
+    }, 750, 'easeInOutExpo')
+
+    e.preventDefault()
+})
+
+// Gallery
+$('.figure').on('click', function() {
+    $('.galleryfocus').css({
+        "opacity" : "1",
+        "display" : "flex",
+        "z-index" : "99"
+    })
+    $('.galleryfocus img')[0].src = this.children[0].src
+})
+
+$('.galleryfocus').on('click', function() {
+    $('.galleryfocus').css({
+        "opacity": "0",
+        "display": "none",
+        "z-index": "-99"
+    })
+    $('.galleryfocus img')[0].src = ""
 })
